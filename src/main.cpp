a61ad16e3264749c75dd4bfafd820d6f348f3402
solidstatebird <33707046+solidstatebird@users.noreply.github.com>
2023-04-27 23:44:13 -0700

    working with 2 channels

--- src/main.cpp
+++ src/main.cpp
@@ -4,99 +4,122 @@
 #include <AnalogBufferDMA.h>
 
 time_t timer = 0;
+time_t timer_end = 0;
 
 const int channel_1_pin = A4;
 const int channel_2_pin = A5;
 
 ADC *adc = new ADC(); // adc object
-const uint32_t buffer_size = 1600;
+const uint16_t buffer_size = 1600;//32767; //breaks if this is more than 1/2 the limit (32,767) also fails if too small - 1600 seems to be around the minimum
 
 DMAMEM static volatile uint16_t __attribute__((aligned(32)))
 adc0_buffer1[buffer_size];
-DMAMEM static volatile uint16_t __attribute__((aligned(32)))
-adc0_buffer2[buffer_size];
+// DMAMEM static volatile uint16_t __attribute__((aligned(32)))
+// adc0_buffer2[buffer_size];
 DMAMEM static volatile uint16_t __attribute__((aligned(32)))
 adc1_buffer1[buffer_size];
-DMAMEM static volatile uint16_t __attribute__((aligned(32)))
-adc1_buffer2[buffer_size];
+// DMAMEM static volatile uint16_t __attribute__((aligned(32)))
+// adc1_buffer2[buffer_size];
 
 // specifying only one buffer automatically enabes stopping on completion
 AnalogBufferDMA adc0_abdma(adc0_buffer1, buffer_size);
+AnalogBufferDMA adc1_abdma(adc1_buffer1, buffer_size);
 // AnalogBufferDMA adc0_abdma(adc0_buffer1, buffer_size, adc0_buffer2, buffer_size);
 // AnalogBufferDMA adc1_abdma(adc1_buffer1, buffer_size, adc1_buffer2, buffer_size);
 
-void processBuffer(AnalogBufferDMA *analogBuffer);
+void processBuffers(int16_t *, int16_t *);
 
 void setup()
 {
-    Serial.begin(115200);
     while (!Serial && millis() < 5000)
-        ; // Wait for serial for at 5 sec
-
-    Serial.println("begin setup");
+        ; // Wait 5 seconds for serial connection
 
     pinMode(channel_1_pin, INPUT_DISABLE);
     pinMode(channel_2_pin, INPUT_DISABLE);
+
     adc->adc0->setAveraging(0);
-    adc->adc0->setResolution(8);
-    // adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::LOW_SPEED);
-    // adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::LOW_SPEED);
+    adc->adc0->setResolution(12);
     adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED);
     adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED);
 
-    // adc->adc1->setAveraging(0);
-    // adc->adc1->setResolution(8);
-    // adc->adc1->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED);
-    // adc->adc1->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED);
-
-    // setup DMA Channels
-    adc0_abdma.init(adc, ADC_0);
-    // adc1_abdma.init(adc, ADC_1);
+    adc->adc1->setAveraging(0);
+    adc->adc1->setResolution(12);
+    adc->adc1->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED);
+    adc->adc1->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED);
 
     // startSynchronizedContinuous omits waiting for calibration
     adc->adc0->wait_for_cal();
-    // adc->adc1->wait_for_cal();
-    Serial.println("end setup");
-    // Start the DMA operation
-    adc->adc0->startContinuous(channel_1_pin);
-    // adc->adc1->startContinuous(channel_2_pin);
+    adc->adc1->wait_for_cal();
 
-    // adc->startSynchronizedContinuous(channel_1_pin, channel_2_pin);
+
+    // setup DMA Channels
+    adc0_abdma.init(adc, ADC_0);
+    adc1_abdma.init(adc, ADC_1);
+
+    Serial.println("end setup");
+
+    // Start the DMA operation
+    adc->startSynchronizedContinuous(channel_1_pin, channel_2_pin);
     timer = micros();
 }
 
 void loop()
 {
-    if (adc0_abdma.interrupted())
+    static bool data_ready = false;
+    static int16_t channel_1_data[buffer_size];
+    static int16_t channel_2_data[buffer_size];
+
+    if (adc0_abdma.interrupted() && adc1_abdma.interrupted()) 
     {
-        processBuffer(&adc0_abdma);
         Serial.println();
-        Serial.print("Time elasped: ");
-        Serial.print(micros() - timer);
-        Serial.println(" us");
+        Serial.print("Sample rate: ");
+        Serial.print(1 / ((float)(micros() - timer) / buffer_size));
+        Serial.println(" Msps");
+
+        processBuffers(channel_1_data, channel_2_data);
+        timer = micros();
+        data_ready = true;
 
         // static int count = 100;
         // if (count > 5) while(1);
         // count++;
-        delay(500);
-        timer = micros();
-        adc0_abdma.init(adc, ADC_0);
-        adc->adc0->startContinuous(channel_1_pin);
+        delay(1000);
+        
+    }
+
+    if (data_ready)
+    {
+        for (uint16_t i = 0; i < buffer_size; i++)
+        {
+            Serial.print(channel_1_data[i]);
+            Serial.print(',');
+            Serial.println(channel_2_data[i]);
+        }
+        data_ready = false;
     }
 }
 
-void processBuffer(AnalogBufferDMA *analogBuffer)
+void processBuffers(int16_t* out1, int16_t* out2)
 {
-    volatile uint16_t *data = analogBuffer->bufferLastISRFilled();
-    volatile uint16_t *end_data = data + analogBuffer->bufferCountLastISRFilled();
-    uint16_t size = analogBuffer->bufferCountLastISRFilled();
+    volatile uint16_t *in0 = adc0_abdma.bufferLastISRFilled();
+    volatile uint16_t *in1 = adc1_abdma.bufferLastISRFilled();
 
-    arm_dcache_delete((void *)data, sizeof(adc0_buffer1));
+    // use if switching buffers
+    // volatile uint16_t size0 = adc0_abdma.bufferCountLastISRFilled();
+    // volatile uint16_t *end_data = data + analogBuffer->bufferCountLastISRFilled();
 
-    for (unsigned int i = 0; i < size; i++)
+    arm_dcache_delete((void *)in0, sizeof(adc0_buffer1));
+    arm_dcache_delete((void *)in1, sizeof(adc0_buffer1));
+
+    for (uint16_t i = 0; i < buffer_size; i++)
     {
-        Serial.println(data[i]);
+        out1[i] = ((int)in0[i] - 2047) * ((3.3 * 1000) / 4096);
+        out2[i] = ((int)in1[i] - 2047) * ((3.3 * 1000) / 4096);
     }
 
-    analogBuffer->clearInterrupt();
+    adc0_abdma.clearInterrupt();
+    adc1_abdma.clearInterrupt();
+    //restart DMA
+    adc0_abdma.clearCompletion();
+    adc1_abdma.clearCompletion();
 }
