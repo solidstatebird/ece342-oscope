a448d8228ea1664f0b2a2d77b6c973fad1027564
cole4miller <milleco7@oregonstate.edu>
2023-04-29 14:19:16 -0700

    All 4 parameters working, count by 4

--- src/main.cpp
+++ src/main.cpp
@@ -22,35 +22,95 @@
 //Encoder knobRight(7, 8);
 Encoder knob(1, 2 );
 
-volatile int position = 0;
+volatile int oldPosition;
 volatile int newPosition;
+
+volatile int position0;
+volatile int position1;
+volatile int position2;
+volatile int position3;
+
 volatile int mode;
 
 //   avoid using pins with LEDs attached
 
+void displayMode() {
+   Serial.print("Mode: ");
+   if (mode == 0) {
+      Serial.print("horizontal scale");
+   }
+   else if (mode == 1) {
+      Serial.print("vertical scale 1");
+   }
+   else if (mode == 2) {
+      Serial.print("vertical scale 2");
+   }
+   else if (mode == 3) {
+      Serial.print("trigger");
+   }
+   else {}
+   Serial.println();
+}
+
+void displayPosition() {
+   Serial.print("horz: ");
+   Serial.print(position0);
+   Serial.print("\t vert1: ");
+   Serial.print(position1);
+   Serial.print("\t vert2: ");
+   Serial.print(position2);
+   Serial.print("\t trig: ");
+   Serial.println(position3);
+}
+
 void rotate() {
-   /*
-   if (digitalRead(B)) {
-      position++;
-      Serial.print(position);
-      Serial.println();
-   }
-   else {
-      position--;
-      Serial.print(position);
-      Serial.println();
-   }
-   */
-   
    int knobValue = knob.read();
-   newPosition = (knobValue / 4);
-   
-   if (newPosition != position) {
-      Serial.print("Knob: ");
-      Serial.print(newPosition);
-      Serial.println();
-      position = newPosition;
+   //newPosition = (knobValue / 4);
+   newPosition = knobValue;
+   int diff = newPosition - oldPosition;
+   if (diff != 0) {
+      if (mode == 0) {
+         position0 = position0 + diff;
+         if (position0 < 1) {
+            position0 = 0;
+         }
+         else if (position0 > 999) {
+            position0 = 1000;
+         }
+      }
+      else if (mode == 1) {
+         position1 = position1 + diff;
+         if (position1 < 1) {
+            position1 = 0;
+         }
+         else if (position1 > 999) {
+            position1 = 1000;
+         }
+      }
+      else if (mode == 2) {
+         position2 = position2 + diff;
+         if (position2 < 1) {
+            position2 = 0;
+         }
+         else if (position2 > 999) {
+            position2 = 1000;
+         }
+      }
+      else if (mode == 3) {
+         position3 = position3 + diff;
+         if (position3 < 1) {
+            position3 = 0;
+         }
+         else if (position3 > 999) {
+            position3 = 1000;
+         }
+      }
+      else {
+      }
+      oldPosition = newPosition;
+      displayPosition();
    }
+   
    
 }
 
@@ -61,9 +121,7 @@ void modeChange() {
    else {
       mode++;
    }
-   Serial.print("mode: ");
-   Serial.print(mode);
-   Serial.println();
+   displayMode();
    delayMicroseconds(50);
 }
 
@@ -80,9 +138,17 @@ void setup() {
 
    attachInterrupt(digitalPinToInterrupt(0), modeChange, FALLING);
 
-   newPosition = knob.read();
-   mode = 0;
+   oldPosition = knob.read();
+   newPosition = oldPosition;
 
+   mode = 0;
+   position0 = 500;
+   position1 = 500;
+   position2 = 500;
+   position3 = 500;
+
+   displayMode();
+   displayPosition();
 }
 
 void loop() {
