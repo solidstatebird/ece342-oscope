20b00b035c06e5acd49268e8dd179c0e8aa50c09
cole4miller <milleco7@oregonstate.edu>
2023-05-10 15:47:26 -0700

    Added display data, mode selection, center trigger

--- src/main.cpp
+++ src/main.cpp
@@ -8,155 +8,620 @@
 #define TFT_DC  9
 #define TFT_CS 10
 
-volatile uint16_t trigger = 40000;
-volatile uint16_t hscale = 97;
+volatile uint16_t mode = 1;             // mode: 1 - 5
+volatile uint16_t trigger = 2;         // trigger: 1 - 50
+volatile uint16_t hscale = 95;          // hscale: 1 - 150
+volatile uint16_t runStop = 0;          // runStop: 0 - 1
 volatile uint16_t testarray[32000];
+volatile uint16_t testarray2[32000];      
 volatile uint16_t frequency;
-//volatile uint16_t vscale1 = 10;
+volatile uint16_t vscale1 = 1;          // vscale1: 1 - 20
+volatile uint16_t vscale2 = 1;          // vscale2: 1 - 20
+volatile uint16_t trigPoint1 = 0;
+volatile uint16_t trigPoint2 = 0;
 
 ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
 
 void sinewave(long freq) {
-  //uint16_t testarray[32000];
   long sineval;
+  
   for (int i = 0; i < 32000; i++) {
     // sine wave for given freq  (max input amplitude = 32767), shifted half way up of max uint16 value (65535)
-    sineval = (16000 * sin(((freq * i) / 1500000.0) * 2.0 * PI));
-    testarray[i] = (sineval + 32767.5); 
+    sineval = (10000.0 * sin(((freq * i) / 1300000.0) * 2.0 * PI));
+    testarray[i] = (sineval + 32767); 
   }
-  //return testarray;
 }
 
-int triggerStart(int trig) {
+void sinewave2(long freq) {
+  long sineval;
+  
+  for (int i = 0; i < 32000; i++) {
+    // sine wave for given freq  (max input amplitude = 32767), shifted half way up of max uint16 value (65535)
+    sineval = (6000.0 * sin(((freq * i) / 1300000.0) * 2.0 * PI + 1.0));
+    testarray2[i] = (sineval + 32767); 
+  }
+}
+
+int triggerStart() {
   int i = 0;
-  while (testarray[i] < trig && i < 32000) {
+  int trig = ((trigger / 50.0) * 32767.0) + 32767;
+  while ((((testarray[i] - 32767.0) * vscale1) + 32767.0) < trig && i < 32000) {
     i++;
   }
   return i;
 }
 
-void drawWave() {
-  //uint16_t testarray[] = sinewave(1000);
-  //tft.fillScreen(ILI9341_BLACK);
+int triggerStart2() {
+  int i = 0;
+  int trig = ((trigger / 50.0) * 32767.0) + 32767;
+  while ((((testarray2[i] - 32767.0) * vscale2) + 32767.0) < trig && i < 32000) {
+    i++;
+  }
+  return i;
+}
 
+int midTrigger() {
+  int trig = ((trigger / 50.0) * 32767.0) + 32767;
+  if (hscale < 99) {
+    trigPoint1 = ((100 - hscale) * 151);
+  }
+  else {
+    trigPoint1 = 76;
+  }
+  trigPoint2 = trigPoint1;
+  while ((((testarray[trigPoint1] - 32767.0) * vscale1) + 32767.0) < trig && trigPoint1 < 31000) {
+    trigPoint1++;
+  }
+  while ((((testarray2[trigPoint2] - 32767.0) * vscale2) + 32767.0) < trig && trigPoint2 < 31000) {
+    trigPoint2++;
+  }
+  if (trigPoint1 > trigPoint2 && trigPoint1 < 30000) {
+    return trigPoint1;
+  }
+  else if (trigPoint2 > trigPoint1 && trigPoint2 < 30000) {
+    return trigPoint2;
+  }
+  else if (trigPoint1 < 30000) {
+    return trigPoint1;
+  }
+  else if (trigPoint2 < 30000) {
+    return trigPoint2;
+  }
+  else {
+    return 32000;
+  }
+}
+
+int vertBoundCheck(int vertin) {
+  if (vertin > 211) {
+    vertin = 211;
+  }
+  else if (vertin < 5) {
+    vertin = 5;
+  }
+  return vertin;
+}
+
+void drawIn1(int start) {
   int y1, y2;
-  int j = 1;
-  int start = triggerStart(trigger);
-  //y1 = (240 * (testarray[start] / 65535.0));
-  y1 = (211 - (206.0 * (testarray[start] / 65535.0)));
-  //Serial.println(start);
+  int j = -76;
+  //int start = midTrigger();
 
-  //tft.setCursor(0, 0);
-  //tft.setTextColor(ILI9341_WHITE);  
-  //tft.setFont(Arial_14);
-  //tft.println("Oscilloscope!");
-  //tft.setFont(Arial_10);
-  //tft.println("Look at that sexy sine wave");
-  if (start > 30000) {
-    
+  if (start > 30000 || trigPoint1 > 30000) {  
     return;
   }
-  else if (hscale < 99) {
-    //y1 = (240 * (testarray[start] / 65535.0));
-    for (int i = 10; i < 313; i+=2) {
-      //y1 = (240 * (testarray[start + (i * (100-hscale))] / 65535.0));
-      //y2 = (240 * (testarray[start + (j * (100 - hscale))] / 65535.0));
-      y2 = (211 - (206.0 * (testarray[start + (j * (100 - hscale))] / 65535.0)));
-      //y2 = ((120 * sin(i))+120);
-      tft.drawLine(i, y1, (i+2), y2, ILI9341_WHITE);
+  else if (hscale < 99) { 
+    y1 = (108 - (104.0 * (((testarray[start + (j * (100 - hscale))] - 32767.0) * vscale1)/ 32767.0)));
+    y1 = vertBoundCheck(y1);
+    j++;
+    for (int i = 11; i < 312; i+=2) {
+      y2 = (108 - (104.0 * (((testarray[start + (j * (100 - hscale))] - 32767.0) * vscale1)/ 32767.0)));
+      y2 = vertBoundCheck(y2);
+    
+      tft.drawLine(i, y1, (i+2), y2, CL(224, 204, 27));
+      
       y1 = y2;
       j++;
     }
   }
   else {
-    //y1 = (240 * (testarray[start] / 65535.0));
-    for (int i = 10; i < 313; i+=(hscale - 97)) {
-      //y1 = (240 * (testarray[start + j] / 65535.0));
-      //y2 = (240 * (testarray[start + j] / 65535.0));
-      y2 = (211 - (206.0 * (testarray[start + j] / 65535.0)));
-      //y2 = ((120 * sin(i))+120);
-      tft.drawLine(i, y1, (i + (hscale - 98)), y2, ILI9341_WHITE);
+    j = (-151 / (hscale - 97));
+    y1 = (108 - (104.0 * (((testarray[start + j] - 32767.0) * vscale1)/ 32767.0)));
+    y1 = vertBoundCheck(y1);
+    j++;
+    for (int i = 11; i < (411 - hscale); i+=(hscale - 97)) {
+      y2 = (108 - (104.0 * (((testarray[start + j] - 32767.0) * vscale1)/ 32767.0)));
+      y2 = vertBoundCheck(y2);
+
+      tft.drawLine(i, y1, (i + (hscale - 97)), y2, CL(224, 204, 27));
+      
       y1 = y2;
       j++;
     }
   }
 }
 
-void testFill() {
-  tft.fillScreen(ILI9341_BLACK);
-  delay(2000);
-  tft.fillScreen(ILI9341_WHITE);
-  delay(2000);
+void drawIn2(int start) {
+  int y1, y2;
+  int j = -76;
+  //int start = midTrigger();
+
+  if (start > 30000 || trigPoint2 > 30000) {  
+    return;
+  }
+  else if (hscale < 99) { 
+    y1 = (108 - (104.0 * (((testarray2[start + (j * (100 - hscale))] - 32767.0) * vscale2)/ 32767.0)));
+    y1 = vertBoundCheck(y1);
+    j++;
+    for (int i = 11; i < 312; i+=2) {
+      y2 = (108 - (104.0 * (((testarray2[start + (j * (100 - hscale))] - 32767.0) * vscale2)/ 32767.0)));
+      y2 = vertBoundCheck(y2);
+    
+      tft.drawLine(i, y1, (i+2), y2, CL(52, 214, 201));
+      
+      y1 = y2;
+      j++;
+    }
+  }
+  else {
+    j = (-151 / (hscale - 97));
+    y1 = (108 - (104.0 * (((testarray2[start + j] - 32767.0) * vscale2)/ 32767.0)));
+    y1 = vertBoundCheck(y1);
+    j++;
+    for (int i = 11; i < (411 - hscale); i+=(hscale - 97)) {
+      y2 = (108 - (104.0 * (((testarray2[start + j] - 32767.0) * vscale2)/ 32767.0)));
+      y2 = vertBoundCheck(y2);
+
+      tft.drawLine(i, y1, (i + (hscale - 97)), y2, CL(52, 214, 201));
+      
+      y1 = y2;
+      j++;
+    }
+  }
 }
 
-void setup() {
-  tft.begin();
-  //tft.setClock(60000000);
-  tft.fillScreen(ILI9341_BLACK);
-  //tft.setTextColor(ILI9341_WHITE);
-  //tft.setTextSize(2);
-  frequency = 10000;
-  sinewave(frequency);
-  
-  tft.println("Waiting for Arduino Serial Monitor...");
+void drawWave() {
+  int y1, y2;
+  int j = 1;
+  int start = triggerStart();
 
-  Serial.begin(9600);
-  while (!Serial) ; // wait for Arduino Serial Monitor
-  Serial.println("ILI9341 Test!"); 
-  tft.setRotation(1);
-  Serial.println(tft.width()),
-  Serial.println(tft.height());
-  for (int i = 0; i < 2000; i++) {
-    Serial.println(testarray[i]);
+  y1 = (108 - (104.0 * (((testarray[start] - 32767.0) * vscale1)/ 32767.0)));
+  y1 = vertBoundCheck(y1);
+
+  if (start > 30000) {  
+    return;
   }
+  else if (hscale < 99) { 
+    for (int i = 12; i < 311; i+=2) {
+      y2 = (108 - (104.0 * (((testarray[start + (j * (100 - hscale))] - 32767.0) * vscale1)/ 32767.0)));
+      y2 = vertBoundCheck(y2);
+    
+      tft.drawLine(i, y1, (i+2), y2, CL(224, 204, 27));
+      
+      y1 = y2;
+      j++;
+    }
+  }
+  else {
+    for (int i = 12; i < (410 - hscale); i+=(hscale - 97)) {
+      y2 = (108 - (104.0 * (((testarray[start + j] - 32767.0) * vscale1)/ 32767.0)));
+      y2 = vertBoundCheck(y2);
+
+      tft.drawLine(i, y1, (i + (hscale - 97)), y2, CL(224, 204, 27));
+      
+      y1 = y2;
+      j++;
+    }
+  }
+}
+
+void drawWave2() {
+  int y1, y2;
+  int j = 1;
+  int start = triggerStart2();
   
+  y1 = (108 - (104.0 * (((testarray2[start] - 32767.0) * vscale2)/ 32767.0)));
+  y1 = vertBoundCheck(y1);
+
+  if (start > 30000) {  
+    return;
+  }
+  else if (hscale < 99) { 
+    for (int i = 12; i < 311; i+=2) {
+      y2 = (108 - (104.0 * (((testarray2[start + (j * (100 - hscale))] - 32767.0) * vscale2)/ 32767.0)));
+      y2 = vertBoundCheck(y2);
+    
+      tft.drawLine(i, y1, (i+2), y2, CL(52, 214, 201));
+      
+      y1 = y2;
+      j++;
+    }
+  }
+  else {
+    for (int i = 12; i < (410 - hscale); i+=(hscale - 97)) {
+      y2 = (108 - (104.0 * (((testarray2[start + j] - 32767.0) * vscale2)/ 32767.0)));
+      y2 = vertBoundCheck(y2);
+
+      tft.drawLine(i, y1, (i + (hscale - 97)), y2, CL(52, 214, 201));
+      
+      y1 = y2;
+      j++;
+    }
+  }
 }
 
 void drawTrigger() {
-  int trig = (211 - (206 * trigger / 65535.0));
-  tft.fillTriangle(1, (trig + 4), 1, (trig-4), 8, trig, ILI9341_WHITE);
+  int trig = ((trigger / 50.0) * 32767.0) + 32767;
+  int trigPointer = (211 - (206 * trig / 65535.0));
+  if (mode == 5) {
+    tft.fillTriangle(2, (trigPointer + 6), 2, (trigPointer-6), 8, trigPointer, ILI9341_WHITE);
+  }
+  else {
+    tft.drawTriangle(2, (trigPointer + 6), 2, (trigPointer-6), 8, trigPointer, ILI9341_WHITE);
+  }
+}
+
+void drawGrid() {
+  // horizontal lines
+  //tft.drawFastHLine(10, 3, 304, ILI9341_WHITE);
+  tft.drawFastHLine(10, 4, 304, ILI9341_WHITE);
+  //tft.drawFastHLine(10, 5, 304, ILI9341_WHITE);
+  tft.drawFastHLine(10, 30, 304, CL(50, 50, 50));
+  tft.drawFastHLine(10, 56, 304, CL(50, 50, 50));
+  tft.drawFastHLine(10, 82, 304, CL(50, 50, 50));
+  tft.drawFastHLine(10, 108, 304, CL(100, 100, 100));
+  tft.drawFastHLine(10, 134, 304, CL(50, 50, 50));
+  tft.drawFastHLine(10, 160, 304, CL(50, 50, 50));
+  tft.drawFastHLine(10, 186, 304, CL(50, 50, 50));
+  //tft.drawFastHLine(10, 211, 304, ILI9341_WHITE);
+  tft.drawFastHLine(10, 212, 304, ILI9341_WHITE);
+  //tft.drawFastHLine(10, 213, 304, ILI9341_WHITE);
+ 
+
+  // vertical lines
+  //tft.drawFastVLine(9, 4, 208, ILI9341_WHITE);
+  tft.drawFastVLine(10, 4, 208, ILI9341_WHITE);
+  //tft.drawFastVLine(11, 4, 208, ILI9341_WHITE);
+  tft.drawFastVLine(48, 5, 207, CL(50, 50, 50));
+  tft.drawFastVLine(86, 5, 207, CL(50, 50, 50));
+  tft.drawFastVLine(124, 5, 207, CL(50, 50, 50));
+  tft.drawFastVLine(162, 5, 207, CL(100, 100, 100));
+  tft.drawFastVLine(200, 5, 207, CL(50, 50, 50));
+  tft.drawFastVLine(238, 5, 207, CL(50, 50, 50));
+  tft.drawFastVLine(276, 5, 207, CL(50, 50, 50));
+  //tft.drawFastVLine(313, 4, 208, ILI9341_WHITE);
+  tft.drawFastVLine(314, 4, 208, ILI9341_WHITE);
+  //tft.drawFastVLine(315, 4, 208, ILI9341_WHITE);
+}
+
+double calculateHscale() {
+  double hdiv;
+  if (hscale < 99){
+    hdiv = (((100.0 - hscale) / 1300000.0) * 19.0);
+  }
+  else {
+    hdiv = ((1.0 / 1300000.0) * ((hscale - 97) / 19.0));
+  }
+  //Serial.print(hdiv);
+  return hdiv;
+}
+
+void displayHscale(double hdiv) {
+  tft.setCursor(188, 222);
+  tft.setTextColor(ILI9341_WHITE); 
+  tft.print("Time: ");
+  //tft.print(hdiv, 3);
+  if (hdiv >= 1) {
+    tft.print(hdiv, 1);
+    tft.print(" s");
+  }
+  else if (hdiv < 1 && hdiv >= 0.001) {
+    tft.print((hdiv * 1000.0), 1);
+    tft.print(" ms");
+  }
+  else if (hdiv < 0.001 && hdiv >= 0.000001) {
+    tft.print((hdiv * 1000000.0), 1);
+    tft.print(" us");
+  }
+  else if (hdiv < 0.000001 && hdiv >= 0.000000001) {
+    tft.print((hdiv * 1000000000.0), 1);
+    tft.print(" ns");
+  }
+  else {
+    tft.print((hdiv * 1000000000000.0), 1);
+    tft.print(" ps");
+  }
+}
+
+void displayRunStop() {
+  tft.setFont(Arial_8);
+  if (runStop == 0) {
+    tft.fillRoundRect(284, 216, 27, 10, 2, ILI9341_WHITE);
+    tft.setCursor(288, 217);
+    tft.setTextColor(ILI9341_BLACK); 
+    tft.print("Run");
+    tft.setCursor(287, 228);
+    tft.setTextColor(ILI9341_WHITE); 
+    tft.print("Stop");
+  }
+  else {
+    tft.setCursor(288, 217);
+    tft.setTextColor(ILI9341_WHITE); 
+    tft.print("Run");
+    tft.fillRoundRect(284, 227, 27, 10, 2, ILI9341_WHITE);
+    tft.setCursor(287, 228);
+    tft.setTextColor(ILI9341_BLACK); 
+    tft.print("Stop");
+  }
+}
+
+void displayData() {
+  float div1 = 1.5 / vscale1;
+  float div2 = 1.5 / vscale2;
+  double hdiv =calculateHscale();
+  tft.setFont(Arial_9);
+  
+  tft.setCursor(10, 222);
+  tft.setTextColor(CL(224, 204, 27));  
+  tft.print("CH1: ");
+  
+  if (div1 > 1) {
+    tft.print(div1, 2);
+    tft.print(" V");
+  }
+  else {
+    tft.print((div1 * 1000), 0);
+    tft.print(" mV");
+  }
+  
+  tft.setCursor(98, 222);
+  tft.setTextColor(CL(52, 214, 201)); 
+  tft.print("CH2: ");
+  
+  if (div2 > 1) {
+    tft.print(div2, 2);
+    tft.print(" V");
+  }
+  else {
+    tft.print((div2 * 1000), 0);
+    tft.print(" mV");
+  }
+  
+  displayHscale(hdiv);
+  displayRunStop();
+}
+
+void displayMode() {
+  if (mode == 1) {
+    tft.drawRoundRect(6, 217, 87, 20, 2, ILI9341_WHITE);
+  }
+  else if (mode == 2) {
+    tft.drawRoundRect(94, 217, 87, 20, 2, ILI9341_WHITE);
+  }
+  else if (mode == 3) {
+    tft.drawRoundRect(184, 217, 94, 20, 2, ILI9341_WHITE);
+  }
+  else if (mode == 4) {
+    tft.drawRoundRect(282, 214, 31, 25, 2, ILI9341_WHITE);
+  }
+}
+
+void updateScreen2() {
+  int start = midTrigger();
+  tft.fillScreen(ILI9341_BLACK);
+  drawGrid();
+  drawIn1(start);
+  drawIn2(start);
+  drawTrigger();
+  displayData();
+  displayMode();
+  delay(10);
 }
 
 void updateScreen() {
   tft.fillScreen(ILI9341_BLACK);
-  tft.drawRect(10, 4, 305, 212, ILI9341_WHITE);
-  tft.drawLine(10, 108, 313, 108, ILI9341_WHITE);
+  drawGrid();
   drawWave();
+  drawWave2();
   drawTrigger();
   tft.setCursor(10, 220);
   tft.setTextColor(ILI9341_WHITE);  
-  tft.setFont(Arial_12);
+  tft.setFont(Arial_10);
   tft.println("Control: Horizontal Scale");
   //tft.setFont(Arial_10);
   //tft.println("Look at that sexy sine wave");
-  delay(4);
+  delay(10);
 
 }
 
+void setup() {
+  tft.begin();
+  tft.fillScreen(ILI9341_BLACK);
+
+  frequency = 8000;
+  sinewave(frequency);
+  sinewave2((frequency));
+
+  Serial.begin(9600);
+
+  tft.setRotation(1);
+}
+
 void loop() {
-  while(hscale < 102) { 
-    while(trigger < 44000) {
-      updateScreen();
-      trigger+=800;
+  while (mode == 1) {
+    updateScreen2();
+      while (vscale1 < 5) {
+        vscale1++;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+      while (vscale1 > 1) {
+        vscale1--;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+    mode++;
+  }
+  while (mode == 2) {
+    updateScreen2();
+      while (vscale2 < 5) {
+        vscale2++;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+      while (vscale2 > 1) {
+        vscale2--;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+    mode++;
+  }
+  while (mode == 3) {
+    updateScreen2();
+      while (hscale < 100) {
+        hscale++;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+      while (hscale > 95) {
+        hscale--;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+    mode++;
+  }
+  while (mode == 4) {
+    updateScreen2();
+      while (runStop < 1) {
+        runStop++;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+      while (runStop > 0) {
+        runStop--;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+      while (runStop < 1) {
+        runStop++;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+      while (runStop > 0) {
+        runStop--;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+      while (runStop < 1) {
+        runStop++;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+      while (runStop > 0) {
+        runStop--;
+        for (int i = 0; i < 15; i++) {
+          updateScreen2();
+        }
+      }
+    mode++;
+  }
+  while (mode == 5) {
+    updateScreen2();
+      while (trigger < 15) {
+        trigger++;
+        for (int i = 0; i < 5; i++) {
+          updateScreen2();
+        }
+      }
+      while (trigger > 2) {
+        trigger--;
+        for (int i = 0; i < 5; i++) {
+          updateScreen2();
+        }
+      }
+    mode = 1;
+  }
+  /*
+  while(hscale < 101) { 
+    while(vscale1 < 4) {
+      updateScreen2();
+      vscale1++;
+      frequency+=1000;
+      //trigger++;
+      sinewave(frequency);
     }
-    while(trigger > 36000) {
-      updateScreen();
-      trigger-=800;
+    while(vscale1 > 1) {
+      updateScreen2();
+      vscale1--;
+      frequency-=1000;
+      //trigger--;
+      sinewave(frequency);
     }
     hscale++;
   }
-  while(hscale > 93) {
-    while(trigger < 44000) {
-      updateScreen();
-      trigger+=800;
+  while(hscale > 96) {
+    while(vscale1 < 4) {
+      updateScreen2();
+      vscale1++;
+      frequency+=1000;
+      //trigger++;
+      sinewave(frequency);
     }
-    while(trigger > 36000) {
-      updateScreen();
-      trigger-=800;
+    while(vscale1 > 1) {
+      updateScreen2();
+      vscale1--;
+      frequency-=1000;
+      //trigger--;
+      sinewave(frequency);
     }
     hscale--;
   }
-  
+  while(hscale < 101) { 
+    while(vscale2 < 10) {
+      updateScreen2();
+      vscale2++;
+      frequency+=1000;
+      //trigger++;
+      sinewave2(frequency);
+    }
+    while(vscale2 > 1) {
+      updateScreen2();
+      vscale2--;
+      frequency-=1000;
+      //trigger--;
+      sinewave2(frequency);
+    }
+    hscale++;
+  }
+  while(hscale > 96) {
+    while(vscale2 < 10) {
+      updateScreen2();
+      vscale2++;
+      frequency+=1000;
+      //trigger++;
+      sinewave2(frequency);
+    }
+    while(vscale2 > 1) {
+      updateScreen2();
+      vscale2--;
+      frequency-=1000;
+      //trigger--;
+      sinewave2(frequency);
+    }
+    hscale--;
+  }
+  */
 }
 
 
