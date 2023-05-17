c0bb7d3d99af78562ef8da10bec0c563929c9fc8
solidstatebird <33707046+solidstatebird@users.noreply.github.com>
2023-05-17 16:04:09 -0700

    mostly working

--- include/display.h
+++ include/display.h
@@ -1,19 +1,53 @@
-#ifndef __PQ_H
-#define __PQ_H
+#pragma once
+#include <Arduino.h>
+#include <cmath>
+#include "SPI.h"
+#include "ILI9341_t3n.h"
+#include "ILI9341_t3n_font_Arial.h"
+#include "display.h"
 
-void sinewave(long freq);
-void sinewave2(long freq);
-int midTrigger();
-int vertBoundCheck(int vertin);
-void drawIn1(int start);
-void drawIn2(int start);
-void drawTrigger();
-void drawGrid();
-double calculateHscale();
-void displayHscale(double hdiv);
-void displayRunStop(); 
-void displayData();
-void displayMode();
-void updateScreen2();
+#define TFT_DC  9
+#define TFT_CS 10
+#define TFT_RST 6
+#define TFT_SCK 13
+#define TFT_MISO 12
+#define TFT_MOSI 11
+#define TOUCH_CS  8
 
-#endif
+
+class Display {
+    public:
+    Display(uint16_t *, uint16_t *, ILI9341_t3n *);
+    void update();
+
+    uint16_t trigger = 2;         // trigger: 1 - 50
+
+    private:
+
+    int midTrigger();
+    int vertBoundCheck(int vertin);
+    void drawIn(int, int, uint16_t);
+    void drawTrigger();
+    void drawGrid();
+    double calculateHscale();
+    void displayHscale(double hdiv);
+    void displayRunStop(); 
+    void displayData();
+    void displayMode();
+
+
+    uint16_t *data[2];
+
+    uint16_t mode = 5;             // mode: 1 - 5
+    uint16_t hscale = 97;          // hscale: 1 - 150
+    uint16_t runStop = 0;          // runStop: 0 - 1
+
+    uint16_t vscale1 = 3;          // vscale1: 1 - 20
+    uint16_t vscale2 = 4;          // vscale2: 1 - 20
+    uint16_t trigPoint1 = 0;
+    uint16_t trigPoint2 = 0;
+
+    const float sampleRate = 1300000.0;
+
+    ILI9341_t3n *tft;
+};
--- src/display.cpp
+++ src/display.cpp
@@ -1,304 +1,299 @@
-#include "display.h"
-#include <Arduino.h>
-#include <cmath>
-#include "SPI.h"
-#include "ILI9341_t3n.h"
-#include "ILI9341_t3n_font_Arial.h"
-/*
-void sinewave(long freq) {
-  long sineval;
-  
-  for (int i = 0; i < 32000; i++) {
-    // sine wave for given freq  (max input amplitude = 32767), shifted half way up of max uint16 value (65535)
-    sineval = (10000.0 * sin(((freq * i) / 1300000.0) * 2.0 * PI));
-    testarray[i] = (sineval + 32767); 
-  }
+#include <display.h>
+
+Display::Display(uint16_t *data1, uint16_t *data2, ILI9341_t3n *screen)
+: tft(screen)
+{
+  data[0] = data1;
+  data[1] = data2;
+  tft->begin();
+  tft->fillScreen(ILI9341_BLACK);
+
+  tft->useFrameBuffer(true);
+
+  tft->setRotation(1);
 }
 
-void sinewave2(long freq) {
-  long sineval;
-  
-  for (int i = 0; i < 32000; i++) {
-    // sine wave for given freq  (max input amplitude = 32767), shifted half way up of max uint16 value (65535)
-    sineval = (6000.0 * sin(((freq * i) / 1300000.0) * 2.0 * PI + 1.0));
-    testarray2[i] = (sineval + 32767); 
-  }
-}
-
-int midTrigger() {
+int Display::midTrigger()
+{
   int trig = ((trigger / 50.0) * 32767.0) + 32767;
-  if (hscale < 99) {
+  if (hscale < 99)
+  {
     trigPoint1 = ((100 - hscale) * 151);
   }
-  else {
+  else
+  {
     trigPoint1 = 76;
   }
   trigPoint2 = trigPoint1;
-  while ((((testarray[trigPoint1] - 32767.0) * vscale1) + 32767.0) < trig && trigPoint1 < 31000) {
+  while ((((data[0][trigPoint1] - 32767.0) * vscale1) + 32767.0) < trig && trigPoint1 < 31000)
+  {
     trigPoint1++;
   }
-  while ((((testarray2[trigPoint2] - 32767.0) * vscale2) + 32767.0) < trig && trigPoint2 < 31000) {
+  while ((((data[1][trigPoint2] - 32767.0) * vscale2) + 32767.0) < trig && trigPoint2 < 31000)
+  {
     trigPoint2++;
   }
-  if (trigPoint1 > trigPoint2 && trigPoint1 < 30000) {
+  if (trigPoint1 > trigPoint2 && trigPoint1 < 30000)
+  {
     return trigPoint1;
   }
-  else if (trigPoint2 > trigPoint1 && trigPoint2 < 30000) {
+  else if (trigPoint2 > trigPoint1 && trigPoint2 < 30000)
+  {
     return trigPoint2;
   }
-  else if (trigPoint1 < 30000) {
+  else if (trigPoint1 < 30000)
+  {
     return trigPoint1;
   }
-  else if (trigPoint2 < 30000) {
+  else if (trigPoint2 < 30000)
+  {
     return trigPoint2;
   }
-  else {
+  else
+  {
     return 32000;
   }
 }
 
-int vertBoundCheck(int vertin) {
-  if (vertin > 211) {
+int Display::vertBoundCheck(int vertin)
+{
+  if (vertin > 211)
+  {
     vertin = 211;
   }
-  else if (vertin < 5) {
+  else if (vertin < 5)
+  {
     vertin = 5;
   }
   return vertin;
 }
 
-void drawIn1(int start) {
+void Display::drawIn(int wave, int start, uint16_t color)
+{
   int y1, y2;
   int j = -76;
 
-  if (start > 30000 || trigPoint1 > 30000) {  
+  if (start > 30000 || trigPoint1 > 30000)
+  {
     return;
   }
-  else if (hscale < 99) { 
-    y1 = (108 - (104.0 * (((testarray[start + (j * (100 - hscale))] - 32767.0) * vscale1)/ 32767.0)));
+  else if (hscale < 99)
+  {
+    y1 = (108 - (104.0 * (((data[wave][start + (j * (100 - hscale))] - 32767.0) * vscale1) / 32767.0)));
     y1 = vertBoundCheck(y1);
     j++;
-    for (int i = 11; i < 312; i+=2) {
-      y2 = (108 - (104.0 * (((testarray[start + (j * (100 - hscale))] - 32767.0) * vscale1)/ 32767.0)));
+    for (int i = 11; i < 312; i += 2)
+    {
+      y2 = (108 - (104.0 * (((data[wave][start + (j * (100 - hscale))] - 32767.0) * vscale1) / 32767.0)));
       y2 = vertBoundCheck(y2);
-    
-      tft.drawLine(i, y1, (i+2), y2, CL(224, 204, 27));
-      
+
+      tft->drawLine(i, y1, (i + 2), y2, color);
+
       y1 = y2;
       j++;
     }
   }
-  else {
+  else
+  {
     j = (-151 / (hscale - 97));
-    y1 = (108 - (104.0 * (((testarray[start + j] - 32767.0) * vscale1)/ 32767.0)));
+    y1 = (108 - (104.0 * (((data[wave][start + j] - 32767.0) * vscale1) / 32767.0)));
     y1 = vertBoundCheck(y1);
     j++;
-    for (int i = 11; i < (411 - hscale); i+=(hscale - 97)) {
-      y2 = (108 - (104.0 * (((testarray[start + j] - 32767.0) * vscale1)/ 32767.0)));
+    for (int i = 11; i < (411 - hscale); i += (hscale - 97))
+    {
+      y2 = (108 - (104.0 * (((data[wave][start + j] - 32767.0) * vscale1) / 32767.0)));
       y2 = vertBoundCheck(y2);
 
-      tft.drawLine(i, y1, (i + (hscale - 97)), y2, CL(224, 204, 27));
-      
+      tft->drawLine(i, y1, (i + (hscale - 97)), y2, color);
+
       y1 = y2;
       j++;
     }
   }
 }
 
-void drawIn2(int start) {
-  int y1, y2;
-  int j = -76;
-
-  if (start > 30000 || trigPoint2 > 30000) {  
-    return;
-  }
-  else if (hscale < 99) { 
-    y1 = (108 - (104.0 * (((testarray2[start + (j * (100 - hscale))] - 32767.0) * vscale2)/ 32767.0)));
-    y1 = vertBoundCheck(y1);
-    j++;
-    for (int i = 11; i < 312; i+=2) {
-      y2 = (108 - (104.0 * (((testarray2[start + (j * (100 - hscale))] - 32767.0) * vscale2)/ 32767.0)));
-      y2 = vertBoundCheck(y2);
-    
-      tft.drawLine(i, y1, (i+2), y2, CL(52, 214, 201));
-      
-      y1 = y2;
-      j++;
-    }
-  }
-  else {
-    j = (-151 / (hscale - 97));
-    y1 = (108 - (104.0 * (((testarray2[start + j] - 32767.0) * vscale2)/ 32767.0)));
-    y1 = vertBoundCheck(y1);
-    j++;
-    for (int i = 11; i < (411 - hscale); i+=(hscale - 97)) {
-      y2 = (108 - (104.0 * (((testarray2[start + j] - 32767.0) * vscale2)/ 32767.0)));
-      y2 = vertBoundCheck(y2);
-
-      tft.drawLine(i, y1, (i + (hscale - 97)), y2, CL(52, 214, 201));
-      
-      y1 = y2;
-      j++;
-    }
-  }
-}
-
-void drawTrigger() {
+void Display::drawTrigger()
+{
   int trig = ((trigger / 50.0) * 32767.0) + 32767;
   int trigPointer = (211 - (206 * trig / 65535.0));
-  if (mode == 5) {
-    tft.fillTriangle(2, (trigPointer + 6), 2, (trigPointer-6), 8, trigPointer, ILI9341_WHITE);
+  if (mode == 5)
+  {
+    tft->fillTriangle(2, (trigPointer + 6), 2, (trigPointer - 6), 8, trigPointer, ILI9341_WHITE);
   }
-  else {
-    tft.drawTriangle(2, (trigPointer + 6), 2, (trigPointer-6), 8, trigPointer, ILI9341_WHITE);
+  else
+  {
+    tft->drawTriangle(2, (trigPointer + 6), 2, (trigPointer - 6), 8, trigPointer, ILI9341_WHITE);
   }
 }
 
-void drawGrid() {
+void Display::drawGrid()
+{
   // horizontal lines
-  
-  tft.drawFastHLine(10, 4, 304, ILI9341_WHITE);
-  tft.drawFastHLine(10, 30, 304, CL(50, 50, 50));
-  tft.drawFastHLine(10, 56, 304, CL(50, 50, 50));
-  tft.drawFastHLine(10, 82, 304, CL(50, 50, 50));
-  tft.drawFastHLine(10, 108, 304, CL(100, 100, 100));
-  tft.drawFastHLine(10, 134, 304, CL(50, 50, 50));
-  tft.drawFastHLine(10, 160, 304, CL(50, 50, 50));
-  tft.drawFastHLine(10, 186, 304, CL(50, 50, 50));
-  tft.drawFastHLine(10, 212, 304, ILI9341_WHITE);
+
+  tft->drawFastHLine(10, 4, 304, ILI9341_WHITE);
+  tft->drawFastHLine(10, 30, 304, CL(50, 50, 50));
+  tft->drawFastHLine(10, 56, 304, CL(50, 50, 50));
+  tft->drawFastHLine(10, 82, 304, CL(50, 50, 50));
+  tft->drawFastHLine(10, 108, 304, CL(100, 100, 100));
+  tft->drawFastHLine(10, 134, 304, CL(50, 50, 50));
+  tft->drawFastHLine(10, 160, 304, CL(50, 50, 50));
+  tft->drawFastHLine(10, 186, 304, CL(50, 50, 50));
+  tft->drawFastHLine(10, 212, 304, ILI9341_WHITE);
 
   // vertical lines
-  tft.drawFastVLine(10, 4, 208, ILI9341_WHITE);
-  tft.drawFastVLine(48, 5, 207, CL(50, 50, 50));
-  tft.drawFastVLine(86, 5, 207, CL(50, 50, 50));
-  tft.drawFastVLine(124, 5, 207, CL(50, 50, 50));
-  tft.drawFastVLine(162, 5, 207, CL(100, 100, 100));
-  tft.drawFastVLine(200, 5, 207, CL(50, 50, 50));
-  tft.drawFastVLine(238, 5, 207, CL(50, 50, 50));
-  tft.drawFastVLine(276, 5, 207, CL(50, 50, 50));
-  tft.drawFastVLine(314, 4, 208, ILI9341_WHITE);
+  tft->drawFastVLine(10, 4, 208, ILI9341_WHITE);
+  tft->drawFastVLine(48, 5, 207, CL(50, 50, 50));
+  tft->drawFastVLine(86, 5, 207, CL(50, 50, 50));
+  tft->drawFastVLine(124, 5, 207, CL(50, 50, 50));
+  tft->drawFastVLine(162, 5, 207, CL(100, 100, 100));
+  tft->drawFastVLine(200, 5, 207, CL(50, 50, 50));
+  tft->drawFastVLine(238, 5, 207, CL(50, 50, 50));
+  tft->drawFastVLine(276, 5, 207, CL(50, 50, 50));
+  tft->drawFastVLine(314, 4, 208, ILI9341_WHITE);
 }
 
-double calculateHscale() {
+double Display::calculateHscale()
+{
   double hdiv;
-  if (hscale < 99){
+  if (hscale < 99)
+  {
     hdiv = (((100.0 - hscale) / sampleRate) * 19.0);
   }
-  else {
+  else
+  {
     hdiv = ((1.0 / sampleRate) * ((hscale - 97) / 19.0));
   }
   return hdiv;
 }
 
-void displayHscale(double hdiv) {
-  tft.setCursor(188, 222);
-  tft.setTextColor(ILI9341_WHITE); 
-  tft.print("Time: ");
-  if (hdiv >= 1) {
-    tft.print(hdiv, 1);
-    tft.print(" s");
+void Display::displayHscale(double hdiv)
+{
+  tft->setCursor(188, 222);
+  tft->setTextColor(ILI9341_WHITE);
+  tft->print("Time: ");
+  if (hdiv >= 1)
+  {
+    tft->print(hdiv, 1);
+    tft->print(" s");
   }
-  else if (hdiv < 1 && hdiv >= 0.001) {
-    tft.print((hdiv * 1000.0), 1);
-    tft.print(" ms");
+  else if (hdiv < 1 && hdiv >= 0.001)
+  {
+    tft->print((hdiv * 1000.0), 1);
+    tft->print(" ms");
   }
-  else if (hdiv < 0.001 && hdiv >= 0.000001) {
-    tft.print((hdiv * 1000000.0), 1);
-    tft.print(" us");
+  else if (hdiv < 0.001 && hdiv >= 0.000001)
+  {
+    tft->print((hdiv * 1000000.0), 1);
+    tft->print(" us");
   }
-  else if (hdiv < 0.000001 && hdiv >= 0.000000001) {
-    tft.print((hdiv * 1000000000.0), 1);
-    tft.print(" ns");
+  else if (hdiv < 0.000001 && hdiv >= 0.000000001)
+  {
+    tft->print((hdiv * 1000000000.0), 1);
+    tft->print(" ns");
   }
-  else {
-    tft.print((hdiv * 1000000000000.0), 1);
-    tft.print(" ps");
-  }
-}
-
-void displayRunStop() {
-  tft.setFont(Arial_8);
-  if (runStop == 0) {
-    tft.fillRoundRect(284, 216, 27, 10, 2, ILI9341_WHITE);
-    tft.setCursor(288, 217);
-    tft.setTextColor(ILI9341_BLACK); 
-    tft.print("Run");
-    tft.setCursor(287, 228);
-    tft.setTextColor(ILI9341_WHITE); 
-    tft.print("Stop");
-  }
-  else {
-    tft.setCursor(288, 217);
-    tft.setTextColor(ILI9341_WHITE); 
-    tft.print("Run");
-    tft.fillRoundRect(284, 227, 27, 10, 2, ILI9341_WHITE);
-    tft.setCursor(287, 228);
-    tft.setTextColor(ILI9341_BLACK); 
-    tft.print("Stop");
+  else
+  {
+    tft->print((hdiv * 1000000000000.0), 1);
+    tft->print(" ps");
   }
 }
 
-void displayData() {
+void Display::displayRunStop()
+{
+  tft->setFont(Arial_8);
+  if (runStop == 0)
+  {
+    tft->fillRoundRect(284, 216, 27, 10, 2, ILI9341_WHITE);
+    tft->setCursor(288, 217);
+    tft->setTextColor(ILI9341_BLACK);
+    tft->print("Run");
+    tft->setCursor(287, 228);
+    tft->setTextColor(ILI9341_WHITE);
+    tft->print("Stop");
+  }
+  else
+  {
+    tft->setCursor(288, 217);
+    tft->setTextColor(ILI9341_WHITE);
+    tft->print("Run");
+    tft->fillRoundRect(284, 227, 27, 10, 2, ILI9341_WHITE);
+    tft->setCursor(287, 228);
+    tft->setTextColor(ILI9341_BLACK);
+    tft->print("Stop");
+  }
+}
+
+void Display::displayData()
+{
   float div1 = 1.5 / vscale1;
   float div2 = 1.5 / vscale2;
-  double hdiv =calculateHscale();
-  tft.setFont(Arial_9);
-  
-  tft.setCursor(10, 222);
-  tft.setTextColor(CL(224, 204, 27));  
-  tft.print("CH1: ");
-  
-  if (div1 > 1) {
-    tft.print(div1, 2);
-    tft.print(" V");
+  double hdiv = calculateHscale();
+  tft->setFont(Arial_9);
+
+  tft->setCursor(10, 222);
+  tft->setTextColor(CL(224, 204, 27));
+  tft->print("CH1: ");
+
+  if (div1 > 1)
+  {
+    tft->print(div1, 2);
+    tft->print(" V");
   }
-  else {
-    tft.print((div1 * 1000), 0);
-    tft.print(" mV");
+  else
+  {
+    tft->print((div1 * 1000), 0);
+    tft->print(" mV");
   }
-  
-  tft.setCursor(98, 222);
-  tft.setTextColor(CL(52, 214, 201)); 
-  tft.print("CH2: ");
-  
-  if (div2 > 1) {
-    tft.print(div2, 2);
-    tft.print(" V");
+
+  tft->setCursor(98, 222);
+  tft->setTextColor(CL(52, 214, 201));
+  tft->print("CH2: ");
+
+  if (div2 > 1)
+  {
+    tft->print(div2, 2);
+    tft->print(" V");
   }
-  else {
-    tft.print((div2 * 1000), 0);
-    tft.print(" mV");
+  else
+  {
+    tft->print((div2 * 1000), 0);
+    tft->print(" mV");
   }
-  
+
   displayHscale(hdiv);
   displayRunStop();
 }
 
-void displayMode() {
-  if (mode == 1) {
-    tft.drawRoundRect(6, 217, 87, 20, 2, ILI9341_WHITE);
+void Display::displayMode()
+{
+  if (mode == 1)
+  {
+    tft->drawRoundRect(6, 217, 87, 20, 2, ILI9341_WHITE);
   }
-  else if (mode == 2) {
-    tft.drawRoundRect(94, 217, 87, 20, 2, ILI9341_WHITE);
+  else if (mode == 2)
+  {
+    tft->drawRoundRect(94, 217, 87, 20, 2, ILI9341_WHITE);
   }
-  else if (mode == 3) {
-    tft.drawRoundRect(184, 217, 94, 20, 2, ILI9341_WHITE);
+  else if (mode == 3)
+  {
+    tft->drawRoundRect(184, 217, 94, 20, 2, ILI9341_WHITE);
   }
-  else if (mode == 4) {
-    tft.drawRoundRect(282, 214, 31, 25, 2, ILI9341_WHITE);
+  else if (mode == 4)
+  {
+    tft->drawRoundRect(282, 214, 31, 25, 2, ILI9341_WHITE);
   }
 }
 
-void updateScreen2() {
+void Display::update()
+{
   int start = midTrigger();
-  tft.fillScreen(ILI9341_BLACK);
+  tft->fillScreen(ILI9341_BLACK);
   drawGrid();
-  drawIn1(start);
-  drawIn2(start);
+  drawIn(0, start, CL(255, 255, 0));
+  drawIn(1, start, CL(0, 255, 255));
   drawTrigger();
   displayData();
   displayMode();
-  delay(5);
-  tft.updateScreen();
+  // delay(1);
+  tft->updateScreen();
 }
-*/
