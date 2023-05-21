5248e7a3f1e9a6dd2b02f3edcc1e8aa8d7846726
cole4miller <milleco7@oregonstate.edu>
2023-05-20 20:33:20 -0700

    added encoder input & trigger channel select

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <Arduino.h>
+#include <Encoder.h>
 #include "display.h"
 #include "sampler.h"
+#include "input.h"
+
 
 const float user_scale_1 = 1.0;
 const float user_scale_2 = 1.0;
 
 const int channel_1_pin = A4;
 const int channel_2_pin = A5;
+
+const int knob_in_1 = 35;
+const int knob_in_2 = 34;
+const int knob_push = 33;
 
 int16_t channel_1_data[BUFFER_SIZE];
 int16_t channel_2_data[BUFFER_SIZE];
@@ -16,6 +23,19 @@ uint16_t screen_channel_2_data[BUFFER_SIZE];
 ILI9341_t3n tft = ILI9341_t3n(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_SCK, TFT_MISO);
 Display *display;
 
+Encoder knob(knob_in_1, knob_in_2);
+KnobIn knob_in;
+
+void rotate() 
+{
+    knob_in.rotate(display, knob);
+}
+
+void modeChange() 
+{
+    knob_in.modeChange(display);
+}
+
 void setup()
 {
     while (!Serial && millis() < 5000)
@@ -23,6 +43,15 @@ void setup()
     display = new Display(screen_channel_1_data, screen_channel_2_data, &tft);
 
     setupADC(channel_1_pin, channel_2_pin);
+
+    pinMode(knob_in_1, INPUT_PULLUP);
+    pinMode(knob_in_2, INPUT_PULLUP);
+    pinMode(knob_push, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(knob_in_2), rotate, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(knob_push), modeChange, FALLING);
+
+    knob_in.oldPosition = knob.read();
+    knob_in.newPosition = knob_in.oldPosition;
 }
 
 void loop()
